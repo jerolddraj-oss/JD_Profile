ea2bc8270524d21aa17a51d6c682baa6bc63faff
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-06 10:46:52 +0530

    Update networking variables.tf with VNet CIDR variables

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,1 +1,22 @@
-# Networking module variables
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "cidr_block_hub" {
+  type = string
+  # e.g., 10.0.0.0/16
+}
+
+variable "cidr_block_spoke1" {
+  type = string
+  # e.g., 10.1.0.0/16
+}
+
+variable "cidr_block_spoke2" {
+  type = string
+  # e.g., 10.2.0.0/16
+}
