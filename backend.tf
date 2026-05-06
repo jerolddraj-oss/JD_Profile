0476438809917b220cd4921152b1091503a4bb42
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-06 09:18:29 +0530

    Update backend.tf with AzureRM backend configuration

--- backend.tf
+++ backend.tf
@@ -1,1 +1,15 @@
-# Terraform backend configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.x.x"
+    }
+  }
+
+  # Remote backend configuration using Azure Storage Account
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestore123"
+    container_name       = "tfstate"
+  }
+}
