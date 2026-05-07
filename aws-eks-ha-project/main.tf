49ae4c55cfd970eed3400e5c3844171d6f4a7790
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-07 12:14:20 +0530

    Update main Terraform configuration with EKS and Transit Gateway

--- aws-eks-ha-project/main.tf
+++ aws-eks-ha-project/main.tf
@@ -17,3 +17,37 @@ module "west_vpc" {
   vpc_cidr    = var.west_vpc_cidr
   environment = "prod-west"
 }
+
+module "east_eks" {
+  source = "./modules/eks"
+
+  cluster_name       = "eks-east"
+  kubernetes_version = var.eks_version
+  vpc_id             = module.east_vpc.vpc_id
+  subnet_ids         = module.east_vpc.private_subnets
+  environment        = "prod-east"
+}
+
+module "west_eks" {
+  source = "./modules/eks"
+
+  providers = {
+    aws = aws.west
+  }
+
+  cluster_name       = "eks-west"
+  kubernetes_version = var.eks_version
+  vpc_id             = module.west_vpc.vpc_id
+  subnet_ids         = module.west_vpc.private_subnets
+  environment        = "prod-west"
+}
+
+module "transit_gateway" {
+  source = "./modules/transit-gateway"
+
+  east_vpc_id = module.east_vpc.vpc_id
+  west_vpc_id = module.west_vpc.vpc_id
+
+  east_subnets = module.east_vpc.private_subnets
+  west_subnets = module.west_vpc.private_subnets
+}
