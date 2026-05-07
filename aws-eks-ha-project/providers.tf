9cabf510964b901544855bd3f90869881ea03e4d
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-07 12:07:56 +0530

    Add Terraform providers configuration

---
+++ aws-eks-ha-project/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.primary_region
+}
+
+provider "aws" {
+  alias  = "west"
+  region = var.secondary_region
+}
