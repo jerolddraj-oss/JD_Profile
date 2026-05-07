71508c855573eadf13703b74fcd57f2efae78854
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-07 12:13:16 +0530

    Add EKS module configuration

---
+++ aws-eks-ha-project/modules/eks/main.tf
@@ -0,0 +1,39 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.kubernetes_version
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  enable_irsa = true
+
+  cluster_enabled_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 2
+      max_size     = 4
+
+      instance_types = ["t3.large"]
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
