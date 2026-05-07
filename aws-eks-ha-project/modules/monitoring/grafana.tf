be6be3b202212c8b1700ad4c3e010259bdf49b54
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-07 12:17:51 +0530

    Add Grafana Helm deployment

---
+++ aws-eks-ha-project/modules/monitoring/grafana.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = "monitoring"
+
+  create_namespace = true
+}
