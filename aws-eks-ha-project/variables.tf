6dd131490c293390fd695413b14136e27428438c
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-07 12:08:20 +0530

    Add Terraform variables

---
+++ aws-eks-ha-project/variables.tf
@@ -0,0 +1,19 @@
+variable "primary_region" {
+  default = "us-east-1"
+}
+
+variable "secondary_region" {
+  default = "us-west-2"
+}
+
+variable "eks_version" {
+  default = "1.28"
+}
+
+variable "east_vpc_cidr" {
+  default = "10.10.0.0/16"
+}
+
+variable "west_vpc_cidr" {
+  default = "10.20.0.0/16"
+}
