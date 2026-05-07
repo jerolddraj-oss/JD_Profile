5748e4b5b4b7b9059d98023495667b9d7f88291d
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-07 12:20:51 +0530

    Add Velero backup configuration

---
+++ aws-eks-ha-project/modules/backup/velero.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "velero" {
+  name       = "velero"
+  repository = "https://vmware-tanzu.github.io/helm-charts"
+  chart      = "velero"
+  namespace  = "velero"
+
+  create_namespace = true
+}
